88dd5a7b49e5d1837a4c260fd7e98ce98f11c922
sunilsingh115 <sunil.singh115@gmail.com>
2019-12-10 12:32:52 +0530

    4 files modified

--- src/app/Server/server.component.ts
+++ src/app/Server/server.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector : 'app-server',
+  //selector :'[app-server]',
+  //selector : '.app-server',
   templateUrl: '../Server/server.component.html',
+  styleUrls:['../app.component.css']
 })
-export class ServerComponent{
-
+export class ServerComponent implements OnInit{
+  ServerID: number = 10;
+  allowNewServer:boolean = false;
+  ServerStatus: string ='offline'; 
+  constructor(){
+    setTimeout(() =>{      
+      this.allowNewServer = true;      
+    },2000)
+  } 
+  ngOnInit(){
+  }
 }
