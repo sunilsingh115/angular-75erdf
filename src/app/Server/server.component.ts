968e60fb236e5d81f29db2802bb05c6d8ef2a67a
sunilsingh115 <sunil.singh115@gmail.com>
2019-12-10 11:12:22 +0530

    4 files modified

--- src/app/Server/server.component.ts
+++ src/app/Server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector : 'app-server',
-  templateUrl: '../Server/server.component.html'
+  templateUrl: '../Server/server.component.html',
 })
 export class ServerComponent{
 
